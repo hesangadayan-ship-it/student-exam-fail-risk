6f52008cffaaa851693ebd489a50bef454a302cd
hesangadayan-ship-it <hesangadayan@gmail.com>
2026-06-30 13:44:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ if st.button("Predict Risk"):
         gauge={
             "axis":{"range":[0,100]},
 
-            "bar":{"color":"red"},
+            "bar":{"color":"Orange"},
 
             "steps":[
                 {"range":[0,40],"color":"green"},
